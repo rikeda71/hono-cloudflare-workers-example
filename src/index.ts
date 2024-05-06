d46f8bfcd417cc3252dbfe8cbc785cb113a2de19
Ryuya Ikeda <rikeda71@gmail.com>
2024-05-06 22:51:20 +0900

    feat: implement routing

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,67 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { drizzle } from "drizzle-orm/d1";
+import { Hono } from "hono";
+import { todos } from "../db/schema";
+import { eq } from "drizzle-orm";
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+const app = new Hono<{ Bindings: Bindings }>().basePath("/api");
+
+app.get("/todos/:id", async (context) => {
+	const id = Number.parseInt(context.req.param("id"));
+	try {
+		const db = drizzle(context.env.DB);
+		const results = await db.select().from(todos).where(eq(todos.id, id));
+		return context.json(results, 200);
+	} catch (error) {
+		return context.json({ error: error }, 500);
+	}
+});
+
+app.get("/todos", async (context) => {
+	try {
+		const db = drizzle(context.env.DB);
+		const results = await db.select().from(todos);
+		return context.json(results, 200);
+	} catch (error) {
+		return context.json({ error: error }, 500);
+	}
+});
+
+app.post("/todos", async (context) => {
+	const todo = await context.req.json<typeof todos.$inferInsert>();
+	try {
+		const db = drizzle(context.env.DB);
+		await db.insert(todos).values(todo);
+		return context.json({ message: "Success." }, 201);
+	} catch (error) {
+		return context.json({ error: error }, 500);
+	}
+});
+
+app.put("/todos/:id", async (context) => {
+	const id = Number.parseInt(context.req.param("id"));
+	const { todo, isDone, deadline } =
+		await context.req.json<typeof todos.$inferInsert>();
+	try {
+		const db = drizzle(context.env.DB);
+		await db
+			.update(todos)
+			.set({ todo, isDone, deadline })
+			.where(eq(todos.id, id));
+		return context.json({ message: "Success." }, 201);
+	} catch (error) {
+		return context.json({ error: error }, 500);
+	}
+});
+
+app.delete("/todos/:id", async (context) => {
+	const id = Number.parseInt(context.req.param("id"));
+	try {
+		const db = drizzle(context.env.DB);
+		await db.delete(todos).where(eq(todos.id, id));
+		return context.json({ message: "Success." }, 201);
+	} catch (error) {
+		return context.json({ error: error }, 500);
+	}
+});
+
+export default app;
